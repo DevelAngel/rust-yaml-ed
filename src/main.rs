9a9d6022ad87200d5c3744619bff8d20b81956c6
Angelos Drossos <develangel@mail.drossos.de>
2023-02-17 22:03:05 +0100

    feat: use yaml-rust instead of serde-yaml

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::fs::File;
-use std::io::BufReader;
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use clap::Parser;
-use serde_yaml::Value;
+use yaml_rust::{YamlEmitter, YamlLoader};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -16,20 +14,19 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let file = File::open(&args.yaml_file)
+    // read
+    let orig: String = std::fs::read_to_string(&args.yaml_file)
         .with_context(|| format!("could not read file: {}", args.yaml_file.display()))?;
-    let object: Value = serde_yaml::from_reader(BufReader::new(file)).with_context(|| {
-        format!(
-            "fail to convert into yaml structure: {}",
-            args.yaml_file.display()
-        )
-    })?;
-    let content = serde_yaml::to_string(&object).with_context(|| {
-        format!(
-            "fail to convert yaml structure to string: {}",
-            args.yaml_file.display()
-        )
-    })?;
-    println!("{}", &content);
+    let root = YamlLoader::load_from_str(&orig)
+        .with_context(|| format!("could not load yaml from: {}", args.yaml_file.display()))?;
+    // write
+    {
+        let mut content = String::new();
+        let mut emitter = YamlEmitter::new(&mut content);
+        emitter
+            .dump(&root[0])
+            .with_context(|| "could not write yaml into string")?;
+        println!("{}", &content);
+    }
     Ok(())
 }
