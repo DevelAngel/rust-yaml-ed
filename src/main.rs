1bccd0c2b53d74649fa5e0958edbb6e9ba89166d
Angelos Drossos <develangel@mail.drossos.de>
2023-02-19 21:20:22 +0100

    feat: use custom loader

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+mod yaml;
+
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use clap::Parser;
-use yaml_rust::{YamlEmitter, YamlLoader};
+
+use crate::yaml::loader::YamlLoader;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -14,19 +17,11 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    // read
     let orig: String = std::fs::read_to_string(&args.yaml_file)
         .with_context(|| format!("could not read file: {}", args.yaml_file.display()))?;
-    let root = YamlLoader::load_from_str(&orig)
-        .with_context(|| format!("could not load yaml from: {}", args.yaml_file.display()))?;
-    // write
-    {
-        let mut content = String::new();
-        let mut emitter = YamlEmitter::new(&mut content);
-        emitter
-            .dump(&root[0])
-            .with_context(|| "could not write yaml into string")?;
-        println!("{}", &content);
-    }
+    let mut content = String::new();
+    let mut loader = YamlLoader::new(&mut content);
+    loader.load_from_str(&orig);
+    println!("{}", &content);
     Ok(())
 }
